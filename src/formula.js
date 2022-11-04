53bdda4967d9a3f25c14e119a41d607ac7f3d5a5
videotutorialdev <12bedevnetwork@gmail.com>
2022-11-05 02:57:41 +0700

    add: formula pagination

---
+++ src/formula.js
@@ -0,0 +1,34 @@
+const perPage = 5;
+const totalRecord = 51;
+const currentPage = 2;
+const size = 6;
+
+const resolvePagination = ({ perPage, totalRecord, currentPage, size }) => {
+  const firstPage = 1;
+  const lastPage = Math.ceil(totalRecord / perPage);
+
+  const startPage =
+    currentPage - Math.floor(size / 2) + (size % 2 === 0 ? 1 : 0);
+  const endPage = currentPage + Math.floor(size / 2);
+
+  return {
+    firstPage,
+    lastPage,
+    startPage:
+      startPage < firstPage
+        ? firstPage
+        : startPage > lastPage - size
+        ? lastPage - size + 1
+        : startPage,
+    endPage:
+      endPage > lastPage
+        ? lastPage
+        : endPage < size
+        ? size > lastPage
+          ? lastPage
+          : size
+        : endPage,
+  };
+};
+
+console.log(resolvePagination({ perPage, currentPage, totalRecord, size }));
