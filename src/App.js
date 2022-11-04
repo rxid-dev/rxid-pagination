14d89a1b4896c0935ca77a9964b8b9dc2fde649d
videotutorialdev <12bedevnetwork@gmail.com>
2022-11-05 00:46:02 +0700

    add: pagination html

--- src/App.js
+++ src/App.js
@@ -11,6 +11,58 @@ function App() {
         <em className="fas fa-plus me-2"></em>
         Add
       </button>
+
+      <div className="text-end">
+        <ul className="pagination-list">
+          <li className="pagination-item">
+            <a className="pagination-link disabled">
+              <em className="fa-solid fa-angles-left" />
+            </a>
+          </li>
+          <li className="pagination-item">
+            <a className="pagination-link disabled">
+              <em className="fas fa-chevron-left" />
+            </a>
+          </li>
+          <li className="pagination-item">
+            <a className="pagination-link ">1</a>
+          </li>
+          <li className="pagination-item">
+            <a className="pagination-link separator">...</a>
+          </li>
+          <li className="pagination-item">
+            <a className="pagination-link">50</a>
+          </li>
+          <li className="pagination-item">
+            <a className="pagination-link">51</a>
+          </li>
+          <li className="pagination-item">
+            <a className="pagination-link active">52</a>
+          </li>
+          <li className="pagination-item">
+            <a className="pagination-link">53</a>
+          </li>
+          <li className="pagination-item">
+            <a className="pagination-link">54</a>
+          </li>
+          <li className="pagination-item">
+            <a className="pagination-link separator">...</a>
+          </li>
+          <li className="pagination-item">
+            <a className="pagination-link">99</a>
+          </li>
+          <li className="pagination-item">
+            <a className="pagination-link">
+              <em className="fas fa-chevron-right" />
+            </a>
+          </li>
+          <li className="pagination-item">
+            <a className="pagination-link">
+              <em className="fa-solid fa-angles-right" />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
